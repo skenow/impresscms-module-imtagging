ece160316a92174f6392ec24d431535fb36f8990
Marc-André Lanciault <marcan@impresscms.org>
2008-11-11 02:27:40 +0000

    Implementing ImtaggingCategoryTreeElement when on user side, as we do not want users to be able to create categories
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imtagging/trunk@6694 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- class/form/elements/imtaggingcategorytreeelement.php
+++ class/form/elements/imtaggingcategorytreeelement.php
@@ -24,6 +24,7 @@ class ImtaggingCategoryTreeElement extends ImtaggingTrayElement {
 
         $itemHandler = isset($object->controls[$key]['itemHandler']) ? $object->controls[$key]['itemHandler'] : 'category';
         $module = isset($object->controls[$key]['module']) ? $object->controls[$key]['module'] : $object->handler->_moduleName;
+        $userside = isset($object->controls[$key]['userside']) ? $object->controls[$key]['userside'] : false;
 		$category_handler = xoops_getmodulehandler('category', $module);
 
 		$category_title_field = $category_handler->identifierName;
@@ -35,41 +36,44 @@ class ImtaggingCategoryTreeElement extends ImtaggingTrayElement {
         include_once(ICMS_ROOT_PATH . "/modules/imtagging/class/icmspersistabletree.php");
         include_once(ICMS_ROOT_PATH . "/modules/imtagging/class/form/elements/imtaggingcategorycheckboxelement.php");
         $mytree = new IcmsPersistableTree($categories, "category_id", "category_pid");
+		$options = $this->getOptionArray($mytree, $category_title_field, 0, "", $ret);
+
         $check_box = new ImtaggingCategoryCheckboxElement(null, $key, $object->getVar($key, 'e'), '<br />');
-
         $ret = array();
-        $options = $this->getOptionArray($mytree, $category_title_field, 0, "", $ret);
         $check_box->addOptionArray($options);
-
         $this->addElement($check_box);
 
-		// on the fly new category creation
-		$new_category_label = new XoopsFormLabel(null, '<a href="#" onclick="jQuery(\'#new_category_tray\').toggle()">' . _CO_IMTAGGING_CATEGORY_ADD . '</a><br />');
-		$this->addElement($new_category_label);
+        // if userside we will use a simple select box
+        if (!$userside) {
+        	// if admin side then we will use the full blown categories control
 
-		// new category container
+			// on the fly new category creation
+			$new_category_label = new XoopsFormLabel(null, '<a href="#" onclick="jQuery(\'#new_category_tray\').toggle()">' . _CO_IMTAGGING_CATEGORY_ADD . '</a><br />');
+			$this->addElement($new_category_label);
 
-		$new_category_tray = new ImtaggingTrayElement(null, null, 'new_category_tray');
+			// new category container
+			$new_category_tray = new ImtaggingTrayElement(null, null, 'new_category_tray');
 
-		// new category title
-		$category_text = new XoopsFormText(null, 'new_category_title', 40, 255);
-		$new_category_tray->addElement($category_text);
+			// new category title
+			$category_text = new XoopsFormText(null, 'new_category_title', 40, 255);
+			$new_category_tray->addElement($category_text);
 
-		// parent select box
-		$parent_cateory_select = new XoopsFormSelect(null, 'category_pid', 0);
-		$parent_options = array(0=>_CO_IMTAGGING_CATEGORY_CATEGORY_PID);
-		foreach ($options as $k=>$v) {
-			$parent_options[$k] = $v;
-		}
-		$parent_cateory_select->addOptionArray($parent_options);
-		$new_category_tray->addElement($parent_cateory_select);
+			// parent select box
+			$parent_cateory_select = new XoopsFormSelect(null, 'category_pid', 0);
+			$parent_options = array(0=>_CO_IMTAGGING_CATEGORY_CATEGORY_PID);
+			foreach ($options as $k=>$v) {
+				$parent_options[$k] = $v;
+			}
+			$parent_cateory_select->addOptionArray($parent_options);
+			$new_category_tray->addElement($parent_cateory_select);
 
-		// new category button
-		$butt_create = new XoopsFormButton('', 'create_button', _CO_ICMS_CREATE, 'button');
-		$butt_create->setExtra('onclick="imtaggingAddCategory();"');
-		$new_category_tray->addElement($butt_create);
+			// new category button
+			$butt_create = new XoopsFormButton('', 'create_button', _CO_ICMS_CREATE, 'button');
+			$butt_create->setExtra('onclick="imtaggingAddCategory();"');
+			$new_category_tray->addElement($butt_create);
 
-		$this->addElement($new_category_tray);
+			$this->addElement($new_category_tray);
+        }
     }
     /**
      * Get options for a category select with hierarchy (recursive)
