8e3fcec34911dbd24e7a2059c11423445c02e1bf
xoops-magazine <xoops-magazine@tld.com>
2008-11-11 15:09:25 +0000

    german translation for imTagging (part 3) 99% finished
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imtagging/trunk@6728 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/german/modinfo.php
+++ language/german/modinfo.php
@@ -22,22 +22,22 @@ define("_MI_IMTAGGING_TAGS", "Tags");
 define("_MI_IMTAGGING_CATEGORIES", "Kategorien");
 
 // Configs
-define("_MI_IMTAGGING_TAGERGR", "Groups allowed to tags");
+define("_MI_IMTAGGING_TAGERGR", "Gruppen die Tags erstellen dürfen");
 define("_MI_IMTAGGING_TAGERGRDSC", "Select the groups which are allowed to create new tags. Please note that a user belonging to one of these groups will be able to tag directly on the site. The module currently has no moderation feature.");
-define("_MI_IMTAGGING_LIMIT", "Tags limit");
-define("_MI_IMTAGGING_LIMITDSC", "Number of tags to display on user side.");
+define("_MI_IMTAGGING_LIMIT", "Tag Limit");
+define("_MI_IMTAGGING_LIMITDSC", "Anzahl der Tags die in der Webseite maximal angezeigt werden sollen.");
 
 // Blocks
-define("_MI_IMTAGGING_TAGRECENT", "Recent tags");
-define("_MI_IMTAGGING_TAGRECENTDSC", "Display most recent tags");
+define("_MI_IMTAGGING_TAGRECENT", "Neue Tags");
+define("_MI_IMTAGGING_TAGRECENTDSC", "Zeigt die jüngsten Tags");
 define("_MI_IMTAGGING_TAGBYMONTH", "Tags nach Monat");
-define("_MI_IMTAGGING_TAGBYMONTHDSC", "Display list of months in which there were tags");
+define("_MI_IMTAGGING_TAGBYMONTHDSC", "Liste der Monate anzeigen, in denen es Tags gab");
 
 // Notifications
 define("_MI_IMTAGGING_GLOBAL_NOTIFY", "Alle Tags");
-define("_MI_IMTAGGING_GLOBAL_NOTIFY_DSC", "Notifications related to all tags in the module");
-define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY", "New tag published");
-define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY_CAP", "Notify me when a new tag is published");
-define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY_DSC", "Receive notification when any new tag is published.");
-define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY_SBJ", "[{X_SITENAME}] {X_MODULE} auto-notify : New tag published");
+define("_MI_IMTAGGING_GLOBAL_NOTIFY_DSC", "Benachrichtigung in Zusammenhang mit allen Tags");
+define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY", "Neuer Tag veröffentlicht");
+define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY_CAP", "Mich benachrichtigen, wenn win neuer Tag veröffentlicht wurde");
+define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY_DSC", "Sende eine Benachrichtigung, sobald ein neuer Tag veröffentlicht wurde.");
+define("_MI_IMTAGGING_GLOBAL_TAG_PUBLISHED_NOTIFY_SBJ", "[{X_SITENAME}] {X_MODULE} auto-notify : Neuer Tag veröffentlicht");
 ?>
